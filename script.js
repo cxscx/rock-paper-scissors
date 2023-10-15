58dd851c1b6c0b6906196d516f491689c9fe6dc5
cxscx <francisco25mcgoncalves@gmail.com>
2023-10-15 23:57:31 +0100

    Fixed bugs
    added return function instead of console.log so value could actually be retrieved and used

--- script.js
+++ script.js
@@ -2,32 +2,51 @@
 // get player to make a choice
 // see which one wins
 
+const choices = ["rock", "paper", "scissors"];
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
-    let choices = ["rock", "paper", "scissors"]
-    let computerChoice = choices[Math.floor(Math.random() * choices.length)]; 
-    console.log(computerChoice);
+   let computerChoice = choices[Math.floor(Math.random() * choices.length)]; 
+   return computerChoice;
 }
 
 function getPlayerChoice() {
-    let choice = prompt("Choose rock, paper or scissors")
-    let playerChoice = choice.toLowerCase()
-    console.log(playerChoice);
+    let choice = prompt("Choose rock, paper or scissors");
+    let playerChoice = choice.toLowerCase();
+    return playerChoice;
 }
 
 const computerSelection = getComputerChoice();
 const playerSelection = getPlayerChoice()
 
-function playRound (playerSelection, computerSelection) {
-    if (playerSelection === "rock" && computerSelection === "scissors") {
-        return("You Win! Rock beats Scissors.");
-    } else if (playerSelection === "paper" && computerSelection === "rock") {
-        return("You Win! Paper beats Rock.");
-    } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        return ("You Win! Scissors beats Paper.");
+
+function playRound(playerSelection, computerSelection) {
+    if ((playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "paper" && computerSelection === "rock") || (playerSelection === "scissors" && computerSelection === "rock")) {
+        return (`You Win! ${playerSelection[0].toUpperCase() + playerSelection.slice(1)} beats ${computerSelection[0].toUpperCase() + computerSelection.slice(1)}.`);
     } else if (playerSelection === computerSelection) {
-        return ("A Tie!");
+        return ("A Tie");
     } else {
-        return (`You Lose! ${computerSelection} beats ${playerSelection}`);
+        return (`You Lose! ${playerSelection[0].toUpperCase() + playerSelection.slice(1) } loses to ${computerSelection[0].toUpperCase() + computerSelection.slice(1)}. `);
     }
 }
-// issues is that computerSelection isn't defined, and should make each round run playerSelection
+console.log(playRound(playerSelection, computerSelection));
+
+function game() {
+    var roundResult = (playRound(playerSelection, computerSelection));
+    if (roundResult.search("You Win!") > -1){
+        playerScore++;
+        return(`Your score is now ${playerScore} VS ${computerScore}.`);
+    } else if (roundResult.search("You Lose!") > -1) {
+        computerScore++;
+        return(`Your score is now ${playerScore} VS ${computerScore}.`)
+    } else {
+        return(`Your score has remained unchanged. Your score is ${playerScore} VS ${computerScore}.`)
+    }
+}
+
+// play round
+// if player wins +1 to player score
+// if computer wins +1 to computer score
+
+
