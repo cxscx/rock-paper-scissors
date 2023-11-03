b9011542572d574b20bbe0128fd39e91b5ad28d0
cxscx <francisco25mcgoncalves@gmail.com>
2023-11-03 22:05:48 +0000

    More changes to game function

--- script.js
+++ script.js
@@ -4,43 +4,43 @@
 
 const choices = ["rock", "paper", "scissors"];
 
+
 function getComputerChoice() {
    let computerChoice = choices[Math.floor(Math.random() * choices.length)]; 
-   console.log(computerChoice);
+   console.log(`Computer chose ${computerChoice}.`);
    return computerChoice;
 }
 
 function getPlayerChoice() {
     let choice = prompt("Choose rock, paper or scissors");
     let playerChoice = choice.toLowerCase();
-    console.log(playerChoice);
+    console.log(`You chose ${playerChoice}.`);
     return playerChoice;
 }
 
 let computerSelection = getComputerChoice();
 let playerSelection = getPlayerChoice()
-
+let computerScore = 0
+let playerScore = 0
 
 function checkWinner(playerSelection, computerSelection) {
     if ((playerSelection === "rock" && computerSelection === "scissors") || 
     (playerSelection === "paper" && computerSelection === "rock") || 
     (playerSelection === "scissors" && computerSelection === "rock")) {
-        return "Player!";
+        return "Player Wins!";
     } else if (playerSelection === computerSelection) {
         return "A Tie";
     } else {
-        return "Computer!";
+        return "Computer Wins!";
     }
 
 }
 
-console.log(checkWinner(playerSelection, computerSelection));
-
 function playRound(playerSelection,computerSelection) {
     const roundResult = (checkWinner(playerSelection, computerSelection));
-    if (roundResult === "Player!"){
+    if (roundResult === "Player Wins!"){
         return `You win this round! ${playerSelection[0].toUpperCase() + playerSelection.slice(1)} beats ${computerSelection[0].toUpperCase() + computerSelection.slice(1)}.`;
-    } else if (roundResult === "Computer!") {
+    } else if (roundResult === "Computer Wins!") {
         return `You lost this round :(. ${playerSelection[0].toUpperCase() + playerSelection.slice(1) } loses to ${computerSelection[0].toUpperCase() + computerSelection.slice(1)}.`;
     } else {
         return "A Tie!";
@@ -49,27 +49,28 @@ function playRound(playerSelection,computerSelection) {
 
 
 function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    console.log("Welcome, please make a choice")
+    console.log("Welcome, please make a choice.")
     for (let i = 0; i < 5; i++) {
         const playerSelection = getPlayerChoice()
         const computerSelection = getComputerChoice()
         console.log(playRound(playerSelection, computerSelection));
-        if (checkWinner === "Player!") {
+        if (checkWinner(playerSelection, computerSelection) === "Player Wins!") {
             playerScore++;
             console.log(`Your score is now ${playerScore} VS ${computerScore}.`);
-        } else if (checkWinner === "Computer!") {
+        } else if (checkWinner(playerSelection, computerSelection) === "Computer Wins!") {
             computerScore++;
             console.log(`Your score is now ${playerScore} VS ${computerScore}.`);
+        } else {
+            console.log (`Your score remains unchanged. It is ${playerScore} VS ${computerScore}.`);
         }
+        console.log("* * * * * * * * * * * * * * * * * * *")
     }
     if (playerScore > computerScore) {
-        console.log(`Congratulations! You Won! The score was ${playerScore} VS ${computerScore}`)
+        console.log(`Congratulations! You Won! The score was ${playerScore} VS ${computerScore}.`)
     } else if (playerScore < computerScore) {
-        console.log(`You Lost! Better luck next time. Your score was ${playerScore} VS ${computerScore}`)
+        console.log(`You Lost :(! Better luck next time. Your score was ${playerScore} VS ${computerScore}.`)
     } else {
-        console.log("We have a Tie!")
+        console.log(`We have a Tie! The score was ${playerScore} VS ${computerScore}.`)
     }
 }
 // play round
